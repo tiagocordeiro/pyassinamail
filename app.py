a2c7a85d3ae75587081b262c78c197175eb6be8b
Tiago Cordeiro <tiago@mulhergorila.com>
2018-06-18 16:11:11 -0300

    PEP8

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import re
 
 def geraAssinaturaHTML():
     file_id = "1Q6zlAQfVKr9Y7Hj2oRXLaWLuwefzc8f1PubEcE2B1gM"
-    url = "https://docs.google.com/spreadsheets/d/{0}/export?format=csv".format(file_id)
+    url = f"https://docs.google.com/spreadsheets/d/{file_id}/export?format=csv"
     arquivo = requests.get(url)
     arquivo.encoding = arquivo.apparent_encoding
     env = jinja2.Environment()
@@ -34,15 +34,21 @@ def geraAssinaturaHTML():
             nomeassinatura = data['Nome']
             nomeassinatura = re.sub('\s+', '', nomeassinatura)
             nomearquivo = remover_acentos(nomeassinatura)
-            arquivoAssinatura = open('assinaturas/' + nomearquivo + '.html', 'w')
-            arquivoAssinatura.write(template.render(data=data, nome=data['Nome'], cargo=data['Cargo'], email=data['Email']))
+            arquivoAssinatura = open('assinaturas/' + nomearquivo + '.html',
+                                     'w')
+            arquivoAssinatura.write(
+                template.render(data=data, nome=data['Nome'],
+                                cargo=data['Cargo'], email=data['Email']))
 
             # Se quiser imprimir na tela, remova o comentário na linha abaixo
-            # print(template.render(data=data, nome=data['Nome'], cargo=data['Cargo'], email=data['Email']))
+            # print(template.render(data=data, nome=data['Nome'],
+            #                       cargo=data['Cargo'], email=data['Email']))
         linha = linha + 1
 
+
 def remover_acentos(txt):
-    return normalize('NFKD', txt).encode('ASCII','ignore').decode('ASCII')
+    return normalize('NFKD', txt).encode('ASCII', 'ignore').decode('ASCII')
+
 
 if __name__ == '__main__':
     geraAssinaturaHTML()
