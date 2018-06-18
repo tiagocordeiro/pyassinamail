357fcde80df9731e94c605e9d6e9366b11c12bc3
Tiago Cordeiro <tiago@mulhergorila.com>
2018-06-18 16:32:05 -0300

    Configura testes

--- tests.py
+++ tests.py
@@ -1,1 +1,9 @@
-from app import remover_acentos
+from .app import remover_acentos
+
+
+def test_remove_acentos():
+    assert remover_acentos("Texto com açêntüs") == "Texto com acentus"
+
+
+def test_template_existe():
+    pass
