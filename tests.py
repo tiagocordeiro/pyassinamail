f9ae024fabfcedb1c6a57e64f5bc4c2c11dd2040
Tiago Cordeiro <tiago@mulhergorila.com>
2018-06-18 17:14:34 -0300

    Melhora testes

--- tests.py
+++ tests.py
@@ -1,9 +1,20 @@
 from .app import remover_acentos
+import os
+
+
+def test_cria_arquivo(tmpdir):
+    p = tmpdir.mkdir("sub").join("assinatura.html")
+    p.write("content")
+    assert p.read() == "content"
+    assert len(tmpdir.listdir()) == 1
 
 
 def test_remove_acentos():
     assert remover_acentos("Texto com açêntüs") == "Texto com acentus"
+    assert remover_acentos("áéíóúâêîôûçüÁçÇ") == "aeiouaeioucuAcC"
+    assert remover_acentos("àèìòùÀÈÌÒÙ") == "aeiouAEIOU"
 
 
 def test_template_existe():
-    pass
+    template_file = os.path.isfile("templates/assinatura.html")
+    assert template_file == True
